e50054e65d263927840cebd390c391d0cf9ab273
Brian Hache <brianjhache@gmail.com>
2024-11-17 14:50:32 -0700

    --

--- resources/views/components/frameworks/tailwind/header/loading.blade.php
+++ resources/views/components/frameworks/tailwind/header/loading.blade.php
@@ -1,6 +1,5 @@
-<div class="fixed inset-0 flex items-center justify-center z-50">
+<div wire:loading class="fixed inset-0 flex items-center justify-center z-50">
     <div
-        wire:loading
         class="absolute inset-0 flex items-center justify-center"
     >
         <svg
